f0f95b4e86cdc137a18dab1e542d8ea8add9ac45
骆邯 <58976235@qq.com>
2021-09-01 10:45:09 +0800

    提交

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,32 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-01 09:33:44
+ * @LastEditTime: 2021-09-01 10:41:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /node_express_template/controllers/index.js
+ */
 const dbconfig = require('../util/dbConfig/dbconfig_admin');
 const TOKEN = require('../util/token/token');
 const { log_out } = require('../public/javascripts/log_out')
 test = (req, res) => {
-    
+    let name=req.query.name
     let Sql = ``;
     let SqlArr = [];
-    let CallBack = (data) => {
+    res.send({
+        name
+    })
+    /* let CallBack = (data) => {
         res.send({
             code: 200,
             data: {
-                data: data,
-                total: data.length
-            }
+                name:name
+            },
+            total: data.length
         })
         log_out.print("info", " 请求信息: " + JSON.stringify(data))
     }
-    dbconfig.sqlConnect(Sql, SqlArr, CallBack);
+    dbconfig.sqlConnect(Sql, SqlArr, CallBack); */
 }
 module.exports={
     test
